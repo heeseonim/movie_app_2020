8711ee3bcde96c81c1c13be22c794ec402df950c
heeseonim <hisundl@naver.com>
2020-01-14 22:34:16 +0900

    Updates

---
+++ precache-manifest.65215363744ed034a075f17651319f95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7889a4c701e666655e41762a0ca92e9d",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a93fb5ed4834e19233c5",
+    "url": "/movie_app_2020/static/css/main.14ea5316.chunk.css"
+  },
+  {
+    "revision": "35cc5f30209bbe41aed2",
+    "url": "/movie_app_2020/static/js/2.c595e5ea.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie_app_2020/static/js/2.c595e5ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a93fb5ed4834e19233c5",
+    "url": "/movie_app_2020/static/js/main.32be7714.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
